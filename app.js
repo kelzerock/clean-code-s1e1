63fccd98b26ecd62d22af22bc25cb9ffefb0c6b6
Aleksei Zhuchkov <kelzzzer@gmail.com>
2022-05-29 21:26:11 +0300

    fix: 3.5 Indents in blocks

--- app.js
+++ app.js
@@ -131,7 +131,7 @@ var taskIncomplete=function(){
     console.log("Incomplete Task...");
 //Mark task as incomplete.
     //When the checkbox is unchecked
-    //Append the task list item to the #incompleteTasks.
+    //Append the task list item to the #incomplete-tasks.
     var listItem=this.parentNode;
     incompleteTaskHolder.appendChild(listItem);
     bindTaskEvents(listItem,taskCompleted);
